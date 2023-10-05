94e32e0d1dc3f7398072d8bd3c17e494387045a2
Bohdan Yereshchenko <y.bogdan.work@gmail.com>
2023-10-05 17:14:01 +0200

    cleaned up client refs

--- serverless/.build/esbuild.config.js
+++
@@ -1,9 +0,0 @@
-// esbuild.config.js
-module.exports = {
-    entryPoints: ["./index.ts"],
-    bundle: true,
-    outfile: "./.build/index.js",
-    platform: "node",
-    target: "node18",
-};
-//# sourceMappingURL=esbuild.config.js.map
