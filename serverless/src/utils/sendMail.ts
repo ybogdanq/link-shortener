c89c4491fe3bb0abf2fc2f5535977cf340d56273
Bohdan Yereshchenko <y.bogdan.work@gmail.com>
2023-10-18 11:28:32 +0200

    Added SES service with v3 commant

--- serverless/src/utils/sendMail.ts
+++ serverless/src/utils/sendMail.ts
@@ -1,4 +1,4 @@
-import { SES } from "@aws-sdk/client-ses";
+import { SES, SendEmailCommand } from "@aws-sdk/client-ses";
 import { createTransport } from "nodemailer";
 import { ses } from "./clients/ses";
 
@@ -6,6 +6,8 @@ const { FROM_EMAIL } = process.env;
 const MAIL_PASS_KEY = "vxquffdytmjbnisx";
 const MAIL_USER = "ybogdanq.mail@gmail.com";
 const SMTP_HOST = "smtp.gmail.com";
+
+//"Throttling: Daily message quota exceeded." So I'm using nodemailer instead
 
 // const transporter = createTransport({
 //   SES: ses,
@@ -23,6 +25,29 @@ const transporter = createTransport({
     rejectUnauthorized: false,
   },
 });
+
+// export const sendMail = async (to: string[], subject: string, text: string) => {
+//   return await ses.send(
+//     new SendEmailCommand({
+//       Source: FROM_EMAIL,
+//       Destination: {
+//         ToAddresses: to,
+//       },
+//       Message: {
+//         Subject: {
+//           Charset: "UTF-8",
+//           Data: "Link deactivated",
+//         },
+//         Body: {
+//           Text: {
+//             Charset: "UTF-8",
+//             Data: text,
+//           },
+//         },
+//       },
+//     })
+//   );
+// };
 
 export const sendMail = async (
   to: string | string[],
